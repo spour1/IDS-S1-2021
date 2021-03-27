1fd0e4540f7cf2ec101278738100f441b80c9566
YunLei Xu <68527784+yunyun0603@users.noreply.github.com>
2021-03-28 11:23:07 +1300

    Add files via upload

---
+++ complete.R
@@ -0,0 +1,22 @@
+complete <- function (directory,id)
+{
+  pathName <- paste(getwd(),"/",directory,sep="")
+  cid=c()
+  cnrow=c()
+  for (i in id)
+  {
+    idx = sprintf("%03d", i)
+    fileName <- paste(pathName,"/",idx,".csv",sep="")
+    x= read.csv(fileName,
+                header = TRUE, 
+                sep = ",", 
+                quote = "\"",
+                dec = ".", 
+                fill = TRUE)
+    w=na.omit(x)
+   cid=c(cid,i)
+   cnrow=c(cnrow,nrow(w))
+  }
+  patientData <- data.frame(id=cid ,nodes=cnrow)
+  patientData
+}
