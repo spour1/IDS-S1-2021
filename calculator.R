0fbfd97a8db7c9ef36e02b133fe3fdaa9cf22eb6
YunLei Xu <68527784+yunyun0603@users.noreply.github.com>
2021-04-26 23:20:24 +1200

    Add files via upload

--- calculator.R
+++ calculator.R
@@ -1,11 +1,11 @@
 calculator <- function (){
-print(gsub("\\\\","","******Simple R Calculator - Select operation: ******"))
-print(gsub("\\\\","","1.Add"))
-print(gsub("\\\\","","2.Subtract"))
-print(gsub("\\\\","","3.Multiply"))
-print(gsub("\\\\","","4.Divide"))
-print(gsub("\\\\","","5.Factors"))
-print(gsub("\\\\","","6.Prime"))
+print("******Simple R Calculator - Select operation: ******")
+print("1.Add")
+print("2.Subtract")
+print("3.Multiply")
+print("4.Divide")
+print("5.Factors")
+print("6.Prime")
 a=readline(prompt="Enter choice [1/2/3/4/5/6]:")
 if (a==1)
 {fadd() }
@@ -19,14 +19,25 @@ else if (a==5)
 {fFactors()}
 else if(a==6)
 {fPrime()}
+else
+{
+  warning("Please type 1-6 to select an operation.")
+}
 }
 fadd <- function (){
   a=as.numeric(readline(prompt="Enter first number:"))
   b=as.numeric(readline(prompt="Enter second number:"))
-  c=paste(a,b,sep="+")
-  d=a+b
-  c=paste(c,"=",d,sep=" ")
-  print(c)
+  {
+    c=paste(a,b,sep="+")
+    d=a+b
+    c=paste(c,"=",d,sep=" ")
+    print(c)
+  }
+  else
+  {
+    print("please enter number.")
+  }
+  
 }
 fSubtract <- function (){
   a=as.numeric(readline(prompt="Enter first number:"))
@@ -50,8 +61,43 @@ fDivide <- function (){
   print(c)
 }
 fFactors <- function (){
-  a=as.numeric(readline(prompt="Enter the number:"))
+  a=as.integer(readline(prompt="Enter the number:"))
+  b=paste("The factors of",a, "are:")
+  print(b)
+  for (i in 1:a)
+  {
+    n=a%%i
+    if (n==0)
+    {
+      print(i)
+    }
+  }
 }
 fPrime <- function (){
-  a=as.numeric(readline(prompt="Enter the number:"))
+  a=as.integer(readline(prompt="Enter the number:"))
+  
+  
+  c=c()
+  for (i in 1:a)
+  {
+    n=a%%i
+    if (n==0)
+    {
+      c=c(c,i)
+    }
+  }
+  if (length(c)>3)
+  {
+    b=paste("The number of",a,"is not a prime.")
+    print(b)
+  }
+    else
+    {
+      b=paste("The number of",a,"is a prime.")
+      print(b)
+    }
+     
 }
+
+#calculator()
+
