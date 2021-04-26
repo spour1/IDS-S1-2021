7c4e488408c0a73eb25930d09218280ac213dc44
YunLei Xu <68527784+yunyun0603@users.noreply.github.com>
2021-04-26 23:18:30 +1200

    Add files via upload

--- corr.R
+++ corr.R
@@ -1,9 +1,14 @@
 corr <- function (directory,threshold=0){
   
   source("complete.R")
-  list1 <- complete("specdata", 1:332)
+  list1 <- complete(directory, 1:332)
   dc<-vector(mode="numeric",length=0)
-  pathName <- paste(getwd(),"/",directory,sep="")
+  #pathName <- paste(getwd(),"/",directory,sep="")
+  pathName<-validationPath(directory)
+  if (is.numeric(threshold))
+  {NULL}
+  else
+  {stop("threshold needs a number.")}
   for (i in 1:nrow(list1))
   {
     fid=list1[i,1]
@@ -14,13 +19,18 @@ corr <- function (directory,threshold=0){
       fid = sprintf("%03d", fid)
       fileName <- paste(pathName,"/",fid,".csv",sep="")
       #print(fileName)
+      if (file.exists(fileName)){
       x= read.csv(fileName,
                   header = TRUE, 
                   sep = ",", 
                   quote = "\"",
                   dec = ".", 
-                  fill = TRUE)
-      
+                  fill = TRUE)}
+      else
+      {
+        warning(fileName," is not exists.")
+        next
+      }
       w = na.omit(x)
       dc=c(dc,cor(w[["sulfate"]],w[["nitrate"]]))
       #print(w[["sulfate"]])
@@ -29,3 +39,15 @@ corr <- function (directory,threshold=0){
   }
   print(dc)
 }
+
+validationPath<- function(directory) 
+{
+  if (dir.exists(directory))
+    directory
+  else
+  {
+    stop("Please put folder specdata under:",getwd())
+  }
+}
+
+#corr('specdata',200)
