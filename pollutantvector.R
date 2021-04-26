cfdc2cd836df22fd7a3476e80821586cc5a009a7
YunLei Xu <68527784+yunyun0603@users.noreply.github.com>
2021-04-26 23:21:31 +1200

    Add files via upload

--- pollutantvector.R
+++ pollutantvector.R
@@ -1,5 +1,13 @@
 pollutantvector <- function (directory, pollutant, id,p){
-  pathName <- paste(getwd(),"/",directory,sep="")
+
+  # 1. directory
+  # 2. pollutant in "sulfate" "nitrate"
+  # 3. id 1:322
+  # 4. p type is number
+  pathName<-validationPath(directory)
+  p_pollutant<-valiPollutant(pollutant)
+  validateID(id)
+  validateP(p)
   y<-c()
   z<-c()
   for (i in id)
@@ -7,12 +15,19 @@ pollutantvector <- function (directory, pollutant, id,p){
     idx = sprintf("%03d", i)
     fileName <- paste(pathName,"/",idx,".csv",sep="")
     #print(fileName)
-    x= read.csv(fileName,
+    if (file.exists(fileName))
+    {x= read.csv(fileName,
                 header = TRUE, 
                 sep = ",", 
                 quote = "\"",
                 dec = ".", 
                 fill = TRUE)
+    }
+    else
+    {
+      warning(fileName," is not exists.")
+      next
+    }
     
     y = c(y,x[[pollutant]])
   }
@@ -26,3 +41,60 @@ pollutantvector <- function (directory, pollutant, id,p){
   }
    z
 }
+
+validationPath<- function(directory) 
+{
+  if (dir.exists(directory))
+    directory
+  else
+  {
+    stop("Please put folder specdata under:",getwd())
+  }
+}
+
+valiPollutant <- function (pollutate) {
+  
+  p_pollutant<-tolower(pollutate)
+  
+  if (p_pollutant == 'nitrate'||pollutate =='sulfate')
+  {
+   p_pollutant
+  }
+  else
+  {
+    stop("Please enter correct value for pollutant:",pollutate)
+    
+  }
+  p_pollutant
+}
+
+validateID<-function(id){
+  n_min <-min(id)
+  n_max <-max(id)
+  if ((1<=n_min)&&(n_min<=3320))
+  {NULL}
+  else
+  {
+    stop("the file id should between 1 to 332 ")
+  }
+  
+  if ((1<=n_max)&&(n_max<=3320))
+  {NULL}
+  else
+  {
+    stop("the file id should between 1 to 332 ")
+  }
+  
+}
+
+validateP <-function(p)
+{
+  if (is.numeric(p))
+    {NULL}
+  else
+    {stop(p," should be numeric")}
+  
+}
+
+#pollutantvector('specdata','sulfate',1:20,12.44)
+pollutantvector("specdata", "sulfate", 1:332,0.5)
